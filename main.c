b9baaf8a28010e830eb95e0ba4a23789d9801ff0
NIUHUI666666 <3926569036@qq.com>
2024-11-19 21:11:09 +0800

    first commit

--- main.c
+++ main.c
@@ -1,5 +1,22 @@
 //请在下方输入你的代码：
+    
+#include<stdio.h>
+#include<math.h>
+
+
 int main()
 {
-    // todo
+    int i, j;
+
+    for (i = 1; i <= 9; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d×%d=%-2d ", j, i, i * j);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
+/*如果i<=j就可以是倒三角*/
